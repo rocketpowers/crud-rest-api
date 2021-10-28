4f8df7343b29c7e641ad80b489c9867d50217226
rocketpowers <rocketpowerswillian@gmail.com>
2021-10-28 15:59:21 -0300

    falta o put

---
+++ src/main/java/com/rocketpowers/Car.java
@@ -0,0 +1,54 @@
+package com.rocketpowers;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	String marca;
+	String modelo;
+	Integer ano;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Integer getAno() {
+		return ano;
+	}
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/rocketpowers/CarController.java
@@ -0,0 +1,52 @@
+package com.rocketpowers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class CarController {
+	
+	
+	@Autowired
+	CarRepository repository;
+
+	@GetMapping("/car")
+	public List<Car> getAllCars(){
+		return repository.findAll();
+		
+	}
+	@GetMapping("/car/{id}")
+	public Car getCarById(@PathVariable Long id) {
+		return repository.findById(id).get();
+		
+	}
+	
+	
+	@PostMapping("/car")
+	public Car saveCar(@RequestBody Car car) {
+		return repository.save(car);
+	}
+	
+	@DeleteMapping("/car/{id}")
+	public void deleteCar(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
+	
+	// @PutMapping
+	//    publ updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+	   //     return jornadaService.updateJornada(jornadaTrabalho);
+	  //  }
+	
+	
+}
