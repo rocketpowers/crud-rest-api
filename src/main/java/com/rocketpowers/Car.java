fc61bcef951d4ee71e9e1a37a07b42c6bda5e886
rocketpowers <rocketpowerswillian@gmail.com>
2021-10-31 09:50:49 -0300

    anotations implements ok

--- src/main/java/com/rocketpowers/Car.java
+++ src/main/java/com/rocketpowers/Car.java
@@ -11,43 +11,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Car {
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getMarca() {
-		return marca;
-	}
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-	public String getModelo() {
-		return modelo;
-	}
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-	public Integer getAno() {
-		return ano;
-	}
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
 	String marca;
 	String modelo;
 	Integer ano;
-	
-	
-	
-	
-	
+
+
+
+
+
 
 }
